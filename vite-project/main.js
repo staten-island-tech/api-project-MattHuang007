f1dd01c5651a3f88d3183184545a040e471bee6b
MattHuang007 <144476342+MattHuang007@users.noreply.github.com>
2023-12-11 13:12:47 -0500

    Update main.js

--- vite-project/main.js
+++ vite-project/main.js
@@ -6,3 +6,22 @@ async function getData(){
   console.log(data);
 }
 getData();
+const URL='https://pokeapi.co/api/v2/';
+
+async function getData(URL){
+    try{
+    const response= await fetch(URL)
+    console.log(response);
+    if(response.status !=200){
+        throw new Error(response.statusText);
+    }
+    //take response from server and convert it to JSON
+    const data= await response.json();
+    document.querySelector("h1").textContent = data.content;
+    document.querySelector("h2").textContent = data.author;
+    }
+    catch(error){
+        document.querySelector("h1").textContent = Error;
+    }
+};
+getData(URL);
